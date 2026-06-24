4b6f442a6383b01a4de1c42dd6fb910eecacec99
Raphael Amorim <rapha850@gmail.com>
2026-06-24 08:20:28 +0200

    start float128 crate

---
+++ crates/float128/src/lib.rs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026-present Raphael Amorim
+ *
+ * This file is part of jam.
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
+ */
+
+// the IEEE-754 binary128 (often called quadruple precision or quad precision)
+// is a standard for representing floating-point numbers in computers.
+
+// for anyone contributing to this (maybe in the future idk):
+// this crate should be fully in rust and dep free
